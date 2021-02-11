e00f3772ce671cbfe9ac0245391579a70006ef9d
mdmufti <67062322+Mufti-Dev@users.noreply.github.com>
2021-02-11 10:02:18 +0530

    Simple string to Integer Conversion

---
+++ Conversion/stringToInt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int stringToInteger(std::string sNum){
+    int result = 0;
+    
+    for(int i = sNum[0] == '-' ? 1 : 0; i < sNum.length(); ++i){
+        int digit = sNum[i] - '0';
+        result = result * 10 + digit;
+    }
+    
+    return sNum[0] == '-' ? -result : result; 
+}
+
+int main() {
+    // Write C++ code here
+    std::string sNum = "999999";
+    std::cout << stringToInteger(sNum) << std::endl;;
+    
+    return 0;
+}
